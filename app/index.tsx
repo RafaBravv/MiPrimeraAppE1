5cf674534d178278c93a10bce0bded8fcd7dddcc
rafa.bravv <rafa.bravv@gmail.com>
2025-09-03 21:16:35 +0000

    📖 Hola mundo!

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,5 @@
+import { WelcomeText } from "@/components/WelcomeText";
+import React from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -10,6 +12,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <WelcomeText/>
     </View>
   );
 }
---
+++ components/WelcomeText.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Text } from "react-native";
+
+export function WelcomeText():any {
+    var list = []
+    return list = [<Text>Hola</Text>, <Text>PEPE</Text>];
+}
